72c78db452fed88728436578407203c4697d0cde
Sourabh Shukla <sourabh01itsme@gmail.com>
2023-02-08 18:49:18 +0530

    done

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const ConnectDatabase = require('./database/db.js')
-
 const app = express();
 const cors = require('cors');
-
-
 const router = require('./Router/Router');
 
 app.use(express.json());
 app.use(cors());
-
 app.use('/', router);
 
 ConnectDatabase()
@@ -20,7 +16,6 @@ ConnectDatabase()
     }).catch((err) => {
         console.log(err.message);
     })
-
 
 
 
