937b872ff23e5a986994fcf78973d3d167e65f46
Sourabh Shukla <sourabh01itsme@gmail.com>
2023-02-08 18:47:37 +0530

    done

--- database/db.js
+++ database/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
 
-async function ConnectDatabase() {
-    let result = await mongoose.connect("mongodb+srv://zostel:zostel123@cluster0.h33fnhs.mongodb.net/?retryWrites=true&w=majority")
+async function ConnectDatabase() 
+{
+    const result = await mongoose.connect("mongodb+srv://zostel:zostel123@cluster0.h33fnhs.mongodb.net/?retryWrites=true&w=majority")
     return result;
 }
-
 module.exports = ConnectDatabase;
